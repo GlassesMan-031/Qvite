2afbb6a6bae0fdf54ee4e42eac1ea51336b7f3d4
Zaher Taeiara <Zaher.t99@gmail.com>
2025-04-29 15:46:32 +0200

    mongoose setup

--- backend/models/transactionModel.js
+++ backend/models/transactionModel.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
-    transactionNote: {
-        type: String,
-        required: true,
-    },
     transactionId: {
         type: Number,
         required: true,
         unique: true
+    },
+    transactionNote: {
+        type: String,
+        required: true,
     },
     transactionImage: {
         type: String

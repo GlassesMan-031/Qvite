036513cef4cf07f30626c8e89cae1ceef443f062
Ingemar Berghult <ingemar.kjellman@gmail.com>
2025-05-06 14:23:32 +0200

    now you can post with mongoose

--- backend/controllers/transactionsController.js
+++ backend/controllers/transactionsController.js
@@ -1,145 +1,161 @@
 const transactionModel = require("../models/transactionModel");
 
-exports.getTransactions = (async (req, res) => {
+exports.getTransactions = async (req, res) => {
   try {
     const allTransactions = await transactionModel.find();
     return res.status(200).json(allTransactions);
-  }
-  catch (error) {
+  } catch (error) {
     return res.status(500).json({
-      error: error.message
+      error: error.message,
     });
   }
+};
 
-});
-
-exports.getTransaction = (async (req, res) => {
+exports.getTransaction = async (req, res) => {
   const { transactionAccountId } = req.params;
   try {
-    const transaction = await transactionModel.find({transcactionId: transcationId});
+    const transaction = await transactionModel.find({
+      transcactionId: transcationId,
+    });
     return res.status(200).json(transaction);
-  }
-  catch (error) {
+  } catch (error) {
     return res.status(500).json({
-      error: error.message
+      error: error.message,
     });
   }
-
-});
+};
 
 //forminput where transaction are made inte the frontend get a prefix with input
-exports.createTransaction = (async(req, res) => {
-    const { transactionAccountId, transactionNote, transactionAmount, transactionImage, transactionReurring, transactionDate } = req.body;
-
-    try {
-        const newTransaction = new transactionModel({
-          transactionAccountId: inputTransactionAccountId,
-          transactionNote: inputTransactionNote,
-          transactionAmount: inputTransactionAmount,
-          transactionImage: inputTransactionImage,
-          transactionRecurring: inputRransactionRecurring,
-          transactionDate: inputTransactionDate
-        });
-        const insertedTransaction = await newTransaction.save();
-        return res.status(201).json(insertedTransaction);
-    }
-    catch (error) {
-        return res.status(500).json({
-            error: error.message
-        });
-    }
-});
-
-
-// update transction, ask jerry how to find  ref ID
-exports.updateTransaction = (async(req, res) => {
-  const {  transactionAccountId, transactionNote, transactionAmount, transactionImage, transactionReurring, transactionDate } = req.body;
+exports.createTransaction = async (req, res) => {
+  const {
+    transactionAccountId,
+    transactionNote,
+    transactionAmount,
+    transactionImage,
+    transactionReurring,
+    transactionDate,
+  } = req.body;
 
   try {
-      await transactionModel.updateOne({ transactionAccountId: inputTransactionAccountId}, {
+    const newTransaction = new transactionModel({
+      transactionAccountId: transactionAccountId,
+      transactionNote: transactionNote,
+      transactionAmount: transactionAmount,
+      transactionImage: transactionImage,
+      transactionRecurring: transactionReurring,
+      transactionDate: transactionDate,
+    });
+    const insertedTransaction = await newTransaction.save();
+    return res.status(201).json(insertedTransaction);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+// update transction, ask jerry how to find  ref ID
+exports.updateTransaction = async (req, res) => {
+  const {
+    transactionAccountId,
+    transactionNote,
+    transactionAmount,
+    transactionImage,
+    transactionReurring,
+    transactionDate,
+  } = req.body;
+
+  try {
+    await transactionModel.updateOne(
+      { transactionAccountId: inputTransactionAccountId },
+      {
         transactionNote: inputTransactionNote,
         transactionAmount: inputTransactionAmount,
         transactionImage: inputTransactionImage,
         transactionRecurring: inputRransactionRecurring,
-        transactionDate: inputTransactionDate
-      });
-      const updatedTransaction = await transactionModel.find({ transactionAccountId: inputTransactionAccountId});
-      return res.status(201).json(updatedTransaction);
+        transactionDate: inputTransactionDate,
+      }
+    );
+    const updatedTransaction = await transactionModel.find({
+      transactionAccountId: inputTransactionAccountId,
+    });
+    return res.status(201).json(updatedTransaction);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
   }
-  catch (error) {
-      return res.status(500).json({
-          error: error.message
-      });
-  }
-});
+};
 
 //delete transaction
 
-exports.deleteTransaction = (async(req, res) => {
-    const { transactionAccountId } = req.params;
-    try {
-        const deletedBook = await transactionModel.deleteOne({ transactionAccountId: inputTransactionAccountId});
-        return res.status(200).json(deletedTransaction)
-    }
-    catch (error) {
-        return res.status(500).json({
-            error: error.message
-        });
-    }
-});
-
-
+exports.deleteTransaction = async (req, res) => {
+  const { transactionAccountId } = req.params;
+  try {
+    const deletedBook = await transactionModel.deleteOne({
+      transactionAccountId: inputTransactionAccountId,
+    });
+    return res.status(200).json(deletedTransaction);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
 
 // get transaction with find-by-ID
 
-exports.getTransactionById = (async(req, res) => {
-    const {  transactionAccountId } = req.params;
-    try {
-        const transaction = await transactionModel.findById( transactionAccountId);
-        return res.status(200).json(transaction);
-    }
-    catch (error) {
-        return res.status(500).json({
-            error: error.message
-        });
-    }
-});
-
+exports.getTransactionById = async (req, res) => {
+  const { transactionAccountId } = req.params;
+  try {
+    const transaction = await transactionModel.findById(transactionAccountId);
+    return res.status(200).json(transaction);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
 
 //alternative functions for create
 
-exports.createTransactionVersionCreate = (async(req, res) => {
-  const {  transactionAccountId, transactionNote, transactionAmount, transactionImage, transactionReurring, transactionDate } = req.body;
+exports.createTransactionVersionCreate = async (req, res) => {
+  const {
+    transactionAccountId,
+    transactionNote,
+    transactionAmount,
+    transactionImage,
+    transactionReurring,
+    transactionDate,
+  } = req.body;
 
-    try {
-        const newTransaction = await transactionModel.create({
-          transactionAccountId: inputTransactionAccountId,
-          transactionNote: inputTransactionNote,
-          transactionAmount: inputTransactionAmount,
-          transactionImage: inputTransactionImage,
-          transactionRecurring: inputRransactionRecurring,
-          transactionDate: inputTransactionDate
-        });
-        return res.status(201).json(newTransaction);
-    }
-    catch (error) {
-        return res.status(500).json({
-            error: error.message
-        });
-    }
-});
+  try {
+    const newTransaction = await transactionModel.create({
+      transactionAccountId: inputTransactionAccountId,
+      transactionNote: inputTransactionNote,
+      transactionAmount: inputTransactionAmount,
+      transactionImage: inputTransactionImage,
+      transactionRecurring: inputRransactionRecurring,
+      transactionDate: inputTransactionDate,
+    });
+    return res.status(201).json(newTransaction);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
 
 //alternative functions for find-by-id-and-delete
 
-exports.deleteTransactionById = (async(req, res) => {
-    const { id } = req.params;
-    try {
-        const deletedTransaction = await transacionModel.findByIdAndDelete(id);
-        return res.status(200).json(deletedTransaction);
-    }
-    catch (error) {
-        return res.status(500).json({
-            error: error.message
-        });
-    }
-});
+exports.deleteTransactionById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedTransaction = await transacionModel.findByIdAndDelete(id);
+    return res.status(200).json(deletedTransaction);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
--- backend/routes/transactionsRoutes.js
+++ backend/routes/transactionsRoutes.js
@@ -1,23 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const transactionController = require('../controllers/transactionController');
+const transactionsController = require("../controllers/transactionsController");
 
-router.get('/user/account/transactions', transactionController.getTransactions);
+router.get(
+  "/user/account/transactions",
+  transactionsController.getTransactions
+);
 
-router.get('/user/account/transactions/:id', transactionController.getTransaction);
+router.get(
+  "/user/account/transactions/:id",
+  transactionsController.getTransaction
+);
 
-router.post('/user/account/transactions', transactionController.createTransaction);
+router.post(
+  "/user/account/transactions",
+  transactionsController.createTransaction
+);
 
-router.put('/user/account/transactions', transactionController.updateTransaction);
+router.put(
+  "/user/account/transactions",
+  transactionsController.updateTransaction
+);
 
-router.delete('/user/account/transactions/:id', transactionController.deleteTransaction);
+router.delete(
+  "/user/account/transactions/:id",
+  transactionsController.deleteTransaction
+);
 
 // Alternative  create endpoint for mongoose
-router.post('/user/account/transactions', transactionController.createTransactionVersionCreate);
+router.post(
+  "/user/account/transactions",
+  transactionsController.createTransactionVersionCreate
+);
 
-// Alternativ för att hämta en bok med obejctid
-router.get('/user/account/transactions-byid/:id', transactionController.getTransactionsById);
-// alternativ för att radera en bok med obectid
-router.delete('/user/account/transactions-byid/:id', transactionController.deleteTransactionById);
+// // Alternativ för att hämta en bok med obejctid
+// router.get(
+//   "/user/account/transactions-byid/:id",
+//   transactionsController.getTransactionsById
+// );
+// // alternativ för att radera en bok med obectid
+// router.delete(
+//   "/user/account/transactions-byid/:id",
+//   transactionsController.deleteTransactionById
+// );
 
 module.exports = router;

8ebca9ee8d5c34ac9d57a0438a4a759bdad8885b
Ingemar Berghult <ingemar.kjellman@gmail.com>
2025-05-07 14:22:05 +0200

    add transactionView

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,22 +1,23 @@
-import {createRouter, createWebHashHistory} from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router";
 
-import HomeView from "./views/HomeView.vue"
-
-import SplashView from "./views/SplashView.vue"
-
-
+import HomeView from "./views/HomeView.vue";
+import SplashView from "./views/SplashView.vue";
+import transactionView from "./views/transactionView.vue";
 
 export default createRouter({
-	history: createWebHashHistory(),
-	routes: [
-		 {
-			component: SplashView,
-			path: "/",
-		}, 
-		{
-			component: HomeView,
-			path: "/home",
-		}
-
-	],
-})
+  history: createWebHashHistory(),
+  routes: [
+    {
+      component: SplashView,
+      path: "/",
+    },
+    {
+      component: transactionView,
+      path: "/transaction",
+    },
+    {
+      component: HomeView,
+      path: "/home",
+    },
+  ],
+});
---
+++ frontend/src/views/transactionView.vue
@@ -0,0 +1,138 @@
+<template>
+  <main class="background">
+    <form action="" method="post">
+      <h1 class="calsans-text">Add Transaction</h1>
+      <div class="container darkerG-Bold">
+        <div class="row align-items-center">
+          <div class="col-auto"><img src="" alt="Icon" /></div>
+          <div class="col-auto"><p class="mb-0">Recurring</p></div>
+          <div class="col-auto"><input type="checkbox" name="recurring" /></div>
+        </div>
+      </div>
+      <div class="container darkerG-Bold">
+        <div class="row align-items-center">
+          <div class="col-auto"><img src="" alt="Icon" /></div>
+          <div class="col-auto"><p class="mb-0">Budget</p></div>
+          <div class="col-auto">
+            <input type="option" name="budget" placeholder="Budget here.." />
+          </div>
+        </div>
+      </div>
+      <div class="container darkerG-Bold">
+        <div class="row align-items-center">
+          <div class="col-auto"><img src="" alt="Icon" /></div>
+          <div class="col-auto"><p class="mb-0">Amount</p></div>
+          <div class="col-auto">
+            <input type="text" name="amount" placeholder="Amount here.." />
+          </div>
+        </div>
+      </div>
+      <div class="container darkerG-Bold">
+        <div class="row align-items-center">
+          <div class="col-auto"><img src="" alt="Icon" /></div>
+          <div class="col-auto"><p class="mb-0">Photo</p></div>
+          <div class="col-auto">
+            <input type="file" @change="onFileSelected" name="photo" />
+          </div>
+        </div>
+      </div>
+      <div class="container darkerG-Bold">
+        <div class="row align-items-center">
+          <div class="col-auto"><img src="" alt="Icon" /></div>
+          <div class="col-auto"><p class="mb-0">Date</p></div>
+          <div class="col-auto"><input type="date" name="date" /></div>
+        </div>
+      </div>
+      <div class="container darkerG-Bold">
+        <div class="row align-items-center">
+          <div class="col-auto"><img src="" alt="Icon" /></div>
+          <div class="col-auto"><p class="mb-0">Notes</p></div>
+          <div class="col-auto">
+            <textarea
+              name="notes"
+              id=""
+              placeholder="Write a note here.."
+            ></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="container darkerG-Bold">
+        <div class="row align-items-center">
+          <div class="col-auto mx-auto">
+            <input class="submitBTN" type="submit" value="SUBMIT" />
+          </div>
+        </div>
+      </div>
+    </form>
+  </main>
+</template>
+<script setup></script>
+<style scoped>
+.background {
+  min-height: 100vh;
+  background-color: #5b8e7d;
+}
+form {
+  background-color: #ebe3d5;
+  border-radius: 12px;
+  padding: 6px;
+  margin: 20px;
+  box-shadow: 0px 7px 15px #2c2a29;
+}
+input,
+textarea {
+  background-color: #776b5d;
+  border-radius: 5px;
+}
+
+input::file-selector-button {
+  background-color: #2c2a29;
+  border-radius: 5px;
+}
+
+.submitBTN {
+  background-color: #2c2a29;
+  border-radius: 5px;
+}
+
+.container {
+  border-bottom: solid 1px #776b5d;
+  padding-bottom: 5px;
+  padding-top: 5px;
+}
+
+.flexDir {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.calsans-text {
+  font-family: "Cal Sans", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  color: #2c2a29;
+}
+
+.darkerG-Bold {
+  font-family: "Darker Grotesque", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: bold;
+  font-style: normal;
+}
+
+.darkerG-Light {
+  font-family: "Darker Grotesque", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 600;
+  font-style: normal;
+  color: #b0a695;
+}
+
+.darkerG-Normal {
+  font-family: "Darker Grotesque", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: normal;
+  font-style: normal;
+}
+</style>

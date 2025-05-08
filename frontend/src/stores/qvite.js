2010e05a0a7b53e763b4c507f8af64ec7f6160cb
Ingemar Berghult <ingemar.kjellman@gmail.com>
2025-05-08 11:28:55 +0200

    add pinia and fetch users

--- frontend/src/stores/qvite.js
+++ frontend/src/stores/qvite.js
@@ -3,13 +3,14 @@ import { defineStore } from "pinia";
 export const useQvite = defineStore("qvite", {
   state: () => ({
     // set variables here
+    users: null,
   }),
   actions: {
     // add global frontend-functions here:
-    async fetchUsers() {
-      const response = await fetch("/api/users");
-      const users = await response.json();
-      return users;
+    async getUsers() {
+      const response = await fetch("http://localhost:3000/api/users");
+      const data = await response.json();
+      console.log(data);
     },
   },
 });

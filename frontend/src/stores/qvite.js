8a14921bb5aee9bb65782063fb9223c41da59157
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2025-05-09 15:33:21 +0200

    started login validation but it doesn't workfor now

--- frontend/src/stores/qvite.js
+++ frontend/src/stores/qvite.js
@@ -1,16 +1,57 @@
 import { defineStore } from "pinia";
 
-export const useQvite = defineStore("qvite", {
+
+
+export let useQvite = defineStore("qvite", {
   state: () => ({
     // set variables here
-    users: null,
+    users: "",
+    usersInfo: "",
+    userName: loginData.username,
+    usersPassword:""
   }),
   actions: {
     // add global frontend-functions here:
     async getUsers() {
-      const response = await fetch("http://localhost:3000/api/users");
-      const users = await response.json();
+      let response = await fetch("http://localhost:3000/api/users");
+      let users = await response.json();
       console.log(users);
     },
+ // shows users credentials in console
+    async showLogin() {
+      try {
+        for (let i = 0; i < this.usersInfo.users.length; i++) {
+          console.log(this.usersInfo.users[i].usersName);
+          console.log(this.usersInfo.users[i].usersPassword);
+        }
+      } catch (error) {
+        console.error(" fetch is fucked up:", error);
+      }
+    },
+  //  Fetches the usercredentials from MySQL
+    async getUsersSplash() {
+      try {
+        const response = await fetch("http://localhost:3000/api/users");
+    
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    
+        this.usersInfo = await response.json();
+    
+        this.showLogin();
+
+        return this.usersInfo;
+      } catch (error) {
+        console.error("Error at user fetch:", error);
+      }
+      
+    },
+
   },
 });
+
+
+
+
+

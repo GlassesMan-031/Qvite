28c66b34c5503b48d9e29ab1cc4317954d947d29
Ingemar Berghult <ingemar.kjellman@gmail.com>
2025-05-09 10:13:04 +0200

    add transaction frontend to backend connection

--- frontend/src/stores/qvite.js
+++ frontend/src/stores/qvite.js
@@ -9,8 +9,8 @@ export const useQvite = defineStore("qvite", {
     // add global frontend-functions here:
     async getUsers() {
       const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
-      console.log(data);
+      const users = await response.json();
+      console.log(users);
     },
   },
 });

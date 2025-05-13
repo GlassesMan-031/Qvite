e199fea2f1665d48c5d502550bb08a8e7771ea37
GlassesMan-031 <karelindqvist@gmail.com>
2025-05-13 10:16:10 +0200

    Add 3x viewpages for settings, change rulings of settingsview

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,13 +3,13 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "./views/homeView.vue";
 import SplashView from "./views/SplashView.vue";
 import transactionView from "./views/transactionView.vue";
-import createView from "./views/createView.vue"
+import createView from "./views/createView.vue";
 import BudgetsView from "./views/BudgetsView.vue";
 import SettingsView from "./views/SettingsView.vue";
 import ScanView from "./views/ScanView.vue";
-
-
-
+import accountSettingsView from "./views/accountSettingsView.vue";
+import budgetSettingsView from "./views/budgetSettingsView.vue";
+import balanceSettingsView from "./views/balanceSettingsView.vue";
 
 // add router name so that we can choose where to display components
 
@@ -34,24 +34,37 @@ export default createRouter({
     {
       component: BudgetsView,
       path: "/budgets",
-      name: "budgets"
+      name: "budgets",
     },
     {
       component: createView,
-			path: "/create",
-      name: "create"
+      path: "/create",
+      name: "create",
     },
     {
       component: SettingsView,
-			path: "/settings",
-      name: "settings"
-    }
-    ,
+      path: "/settings",
+      name: "settings",
+    },
     {
       component: ScanView,
-			path: "/scan",
-      name: "scan"
-    }
-   
+      path: "/scan",
+      name: "scan",
+    },
+    {
+      component: accountSettingsView,
+      path: "/account-settings",
+      name: "account settings",
+    },
+    {
+      component: budgetSettingsView,
+      path: "/budget-settings",
+      name: "budget settings",
+    },
+    {
+      component: balanceSettingsView,
+      path: "/balance-settings",
+      name: "balance settings",
+    },
   ],
 });

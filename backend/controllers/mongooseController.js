0c4664f56a7a84968ab7523dc7f2457744c23253
Kåre Lindqvist <karelindqvist@gmail.com>
2025-05-16 11:41:13 +0200

    Add comments/clean up mongooseController

--- backend/controllers/mongooseController.js
+++ backend/controllers/mongooseController.js
@@ -1,30 +1,33 @@
 const transactionModel = require("../models/transactionModel");
 
+// fetches all transactions
 exports.getTransactions = (async(req, res) => {
     try {
         const allTransaction = await transactionModel.find();
-        return res.status(200).json(allTransaction );
+        return res.status(200).json(allTransaction ); //** successful fetch for all transactions */
     }
     catch (error) {
-        return res.status(500).json({
+        return res.status(500).json({ //! 500 error - server error
             error: error.message
         });
     }
 });
 
+// fetches single transaction
 exports.getTransaction  = (async(req, res) => {
     const { transactionId  } = req.params;
     try {
         const transaction  = await transactionModel.find({ transactionId : transactionId });
-        return res.status(200).json(transaction );
+        return res.status(200).json(transaction );//** successful fetch for single transaction */
     }
     catch (error) {
-        return res.status(500).json({
+        return res.status(500).json({//! 500 error - server error
             error: error.message
         });
     }
 });
 
+// creates new transaction
 exports.createTransaction = (async(req, res) => {
     const { transactionId, transactionNote, transactionImage } = req.body;
 
@@ -36,74 +39,39 @@ exports.createTransaction = (async(req, res) => {
 
         });
         const insertedTransaction= await newTransaction.save();
-        return res.status(201).json(insertedTransaction);
+        return res.status(201).json(insertedTransaction);//** successful create for new transaction*/
     }
     catch (error) {
-        return res.status(500).json({
+        return res.status(500).json({//! 500 error - server error
             error: error.message
         });
     }
 });
 
-
-// exports.updateBook = (async(req, res) => {
-//     const { bookAuthor, bookTitle, bookIsbn, bookPrice, bookCategory1, bookCategory2 } = req.body;
-
-//     try {
-//         await transactionModel.updateOne({isbn: bookIsbn}, {
-//             author: bookAuthor,
-//             book: bookTitle,
-//             isbn: bookIsbn,
-//             price: bookPrice,
-//             categories: [bookCategory1, bookCategory2]
-//         });
-//         const updatedBook = await transactionModel.find({isbn: bookIsbn});
-//         return res.status(200).json(updatedBook);
-//     }
-//     catch (error) {
-//         return res.status(500).json({
-//             error: error.message
-//         });
-//     }
-// });
-
+// deletes single transaction by ID
 exports.deleteTransaction = (async(req, res) => {
     const { id } = req.params;
     try {
         const deletedTransaction = await transactionModel.deleteOne({transactionId: id});
-        return res.status(200).json(deletedTransaction)
+        return res.status(200).json(deletedTransaction) //** successful delete of transaction*/
     }
     catch (error) {
-        return res.status(500).json({
+        return res.status(500).json({//! 500 error - server error
             error: error.message
         });
     }
 });
 
-// Här använder vi findById som är ett tillägg enbart i mongoose
+// here we use findById which is only available in mongoose
 exports.getTransactionById = (async(req, res) => {
     const { id } = req.params;
     try {
         const transaction = await transactionModel.findById(id);
-        return res.status(200).json(transaction);
+        return res.status(200).json(transaction);//** successful fetch by ID*/
     }
     catch (error) {
-        return res.status(500).json({
+        return res.status(500).json({//! 500 error - server error
             error: error.message
         });
     }
 });
-
-// Här använder vi findByIdAndDelete som är ett tillägg enbart i mongoose
-// exports.deleteBookById = (async(req, res) => {
-//     const { id } = req.params;
-//     try {
-//         const deletedBook = await transactionModel.findByIdAndDelete(id);
-//         return res.status(200).json(deletedBook);
-//     }
-//     catch (error) {
-//         return res.status(500).json({
-//             error: error.message
-//         });
-//     }
-// });

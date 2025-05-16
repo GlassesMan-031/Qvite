6896d8f4e6bc05d93713bc6b9814403eea52ff89
Kåre Lindqvist <karelindqvist@gmail.com>
2025-05-16 12:11:34 +0200

    Cleanup/comment on all route files

--- backend/routes/budgetCategoryRoutes.js
+++ backend/routes/budgetCategoryRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/budgetCategoryController");
 
-router.get("/api/budget/categories", controller.getAllCategories);
-router.get("/api/:userName/category/:categoryName", controller.getOneCategory);
-router.post("/api/:userName/category", controller.createCategory);
-router.put("/api/:userName/category/:categoryId", controller.updateCategory);
-router.delete("/api/:userName/category/:categoryId", controller.deleteCategory);
+router.get("/api/budget/categories", controller.getAllCategories); //? fetch all
+router.get("/api/:userName/category/:categoryName", controller.getOneCategory); //? fetch one
+router.post("/api/:userName/category", controller.createCategory); //?give user category
+router.put("/api/:userName/category/:categoryId", controller.updateCategory);//? update user category
+router.delete("/api/:userName/category/:categoryId", controller.deleteCategory);//! delete category from user
 
 module.exports = router;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.get("/api/check-user", userController.checkUserExists);
-router.get("/api/users", userController.getUsers);
-router.get("/api/users/:id", userController.getUser); // see inlogged user at frontend
-router.post("/api/users", userController.createUser);
-router.put("/api/:usersName", userController.updateUser);
-router.delete("/api/:usersName", userController.deleteUser);
-router.get("/api/:usersName", userController.getUserInfo);
+
+router.get("/api/users", userController.getUsers); //? get users
+router.get("/api/users/:id", userController.getUser); //? see logged in user at frontend
+router.post("/api/users", userController.createUser); //? create user 
+router.put("/api/:usersName", userController.updateUser); //? update user
+router.delete("/api/:usersName", userController.deleteUser); //! delete user 
+router.get("/api/:usersName", userController.getUserInfo); //? fetch user by ID
 
 module.exports = router;
